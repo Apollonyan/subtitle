9b8a362bb4da7db55b74272d69f21f062a5ef8bc
ApolloZhu <public-apollonian@outlook.com>
2021-04-01 23:01:49 -0400

    Some VTT have line number :(
    Probably should refactor

--- Sources/vtt/Parser.swift
+++ Sources/vtt/Parser.swift
@@ -21,12 +21,16 @@ extension VTT {
 
         self.init(segments: content.components(separatedBy: "\(lineSeparator)\(lineSeparator)").compactMap { part in
             let lines = part.components(separatedBy: lineSeparator)
-            guard let rawTimestamps = lines.first?.components(separatedBy: " --> "),
-                  rawTimestamps.count == 2
-            else { return nil }
+            guard !lines.isEmpty else { return nil }
+
+            let timestampIndex = !lines[0].contains(" --> ") && lines.count > 1 && lines[1].contains(" --> ") ? 1 : 0
+
+            let rawTimestamps = lines[timestampIndex].components(separatedBy: " --> ")
+            guard rawTimestamps.count == 2 else { return nil }
+
             let start = Self.timeInterval(from: rawTimestamps[0])
             let finish = Self.timeInterval(from: rawTimestamps[1])
-            let contents = Array(lines[1...])
+            let contents = Array(lines[(timestampIndex+1)...])
             guard !contents.isEmpty else { return nil }
             return Segment(startTime: start, endTime: finish, contents: contents)
         })
